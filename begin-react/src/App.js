84074e726198d92bfa9cbf289a8471e02e341566
Junhan0037 <wnsgks0037@naver.com>
2021-06-10 10:35:38 +0900

    feat: props
    - props children

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,22 +1,12 @@
 import Hello from "./Hello";
-import './App.css'
+import Wrapper from "./Wrapper";
 
 function App() {
-    const name = 'react';
-    const style = {
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: 24,
-        padding: '1rem'
-    }
     return (
-        <div>
-            <Hello />
-            <Hello />
-            <Hello />
-            <div style={style}>{name}</div>
-            <div className="gray-box" />
-        </div>
+        <Wrapper>
+            <Hello name="react" color="red" />
+            <Hello color="pink" />
+        </Wrapper>
     );
 }
 
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Hello() {
-    return <div>안녕하세요.</div>
+function Hello({color, name}) {
+    return <div style={{
+        color
+    }}>안녕하세요. {name}</div>
 }
 
+Hello.defaultProps = { // props 기본값
+    name: '이름없음'
+}
 export default Hello;
