2505fc2b61f9aca7ede5702f8869b5e7cbfad121
Junhan0037 <wnsgks0037@naver.com>
2021-06-10 14:18:19 +0900

    feat: 배열 항목 추가하기
    -

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,22 @@
 import UserList from "./UserList";
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
+import CreateUser from "./CreateUser";
 
 function App() {
-    const users = [
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+    });
+    const {username, email} = inputs;
+    const onChange = e => {
+        const {name, value} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value,
+        });
+    };
+
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'abc',
@@ -18,17 +32,31 @@ function App() {
             username: 'ghi',
             email: 'public.ghi@gmail.com',
         },
-    ];
+    ]);
 
     const nextId = useRef(4); // 컴포넌트안의 기억되는 변수 (변수가 바뀐다고해서 리랜더링 X)
 
-   const onCreate = () => {
-        console.log(nextId.current); // 4
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers([...users, user]); // 기존의 users를 복사해서 새항목 추가
+        // setUsers(users.concat(user)); // 위와 같은 기능
+        setInputs({
+            username: '',
+            email: '',
+        });
+
         nextId.current += 1;
     }
 
     return (
-        <UserList users={users} />
+        <>
+            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+            <UserList users={users} />
+        </>
     );
 }
 
