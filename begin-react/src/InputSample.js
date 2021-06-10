2d3bc2d6e10868c76255bea1ca6de04873b1e981
Junhan0037 <wnsgks0037@naver.com>
2021-06-10 13:30:42 +0900

    feat: useRef
    - 특정 DOM 접근하기

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 function InputSample() {
     const [inputs, setInputs] = useState({
         name: '',
         nickname: '',
     });
+    const nameInput = useRef();
     const {name, nickname} = inputs;
 
     const onChange = (e) => {
@@ -20,12 +21,13 @@ function InputSample() {
         setInputs({
             name: '',
             nickname: '',
-        })
+        });
+        nameInput.current.focus(); // DOM 접근
     }
 
     return (
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
