6e11c7eba24663c713df564e5537e21a1beead66
Junhan0037 <wnsgks0037@naver.com>
2021-06-10 13:49:38 +0900

    feat: useRef (컴포넌트 안의 변수)
    -

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,34 @@
 import UserList from "./UserList";
+import {useRef} from 'react';
 
 function App() {
+    const users = [
+        {
+            id: 1,
+            username: 'abc',
+            email: 'public.abc@gmail.com',
+        },
+        {
+            id: 2,
+            username: 'def',
+            email: 'public.def@gmail.com',
+        },
+        {
+            id: 3,
+            username: 'ghi',
+            email: 'public.ghi@gmail.com',
+        },
+    ];
+
+    const nextId = useRef(4); // 컴포넌트안의 기억되는 변수 (변수가 바뀐다고해서 리랜더링 X)
+
+   const onCreate = () => {
+        console.log(nextId.current); // 4
+        nextId.current += 1;
+    }
+
     return (
-        <UserList />
+        <UserList users={users} />
     );
 }
 
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -8,29 +8,12 @@ function User({user}) {
     );
 }
 
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'abc',
-            email: 'public.abc@gmail.com',
-        },
-        {
-            id: 2,
-            username: 'def',
-            email: 'public.def@gmail.com',
-        },
-        {
-            id: 3,
-            username: 'ghi',
-            email: 'public.ghi@gmail.com',
-        },
-    ];
+function UserList({users}) {
 
     return (
         <div>
             {
-                users.map(user => (<User user={user} key={user.id} />)) // key할 값이 없다면 index 삽입
+                users.map(user => (<User user={user} key={user.id} />)) // key할 값이 없다면 index 삽
             }
         </div>
     );
