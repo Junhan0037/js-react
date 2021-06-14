dce1b4f28f842d24f33dcbd3479ce4e556293ee0
Junhan0037 <wnsgks0037@naver.com>
2021-06-14 17:04:35 +0900

    feat: UserDispatch Context
    - Context API

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,5 +1,5 @@
 import UserList from "./UserList";
-import {useRef, useReducer, useMemo, useCallback} from 'react';
+import {useRef, useReducer, useMemo, useCallback, createContext} from 'react';
 import CreateUser from "./CreateUser";
 import useInputs from "./useInputs";
 
@@ -53,6 +53,8 @@ function reducer(state, action) {
     }
 }
 
+export const UserDispatch = createContext(null);
+
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
     const nextId = useRef(4);
@@ -76,28 +78,14 @@ function App() {
         reset();
     }, [username, email, reset]);
 
-    const onToggle = useCallback(id => {
-        dispatch({
-            type: 'TOGGLE_USER',
-            id
-        });
-    }, []);
-
-    const onRemove = useCallback(id => {
-        dispatch({
-            type: 'REMOVE_USER',
-            id
-        });
-    }, []);
-
     const count = useMemo(() => countActiveUsers(users), [users]);
 
     return (
-        <>
+        <UserDispatch.Provider value={dispatch}>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+            <UserList users={users} />
             <div>활성 사용자 수 : {count}</div>
-        </>
+        </UserDispatch.Provider>
     );
 }
 
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,21 +1,23 @@
-import React, {useEffect} from "react";
+import React, {useContext} from "react";
+import {UserDispatch} from "./App";
 
-const User = React.memo(function User({user, onRemove, onToggle}) {
+const User = React.memo(function User({user}) {
     const {username, email, id, active} = user;
+    const dispatch = useContext(UserDispatch);
     return (
         <div>
-            <b style={{color: active ? "green" : "black", cursor: "pointer"}} onClick={() => onToggle(id)}>{username}</b> <span>({email})</span>
-            <button onClick={() => onRemove(id)}>삭제</button>
+            <b style={{color: active ? "green" : "black", cursor: "pointer"}} onClick={() => dispatch({type: 'TOGGLE_USER', id})}>{username}</b> <span>({email})</span>
+            <button onClick={() => dispatch({type: 'REMOVE_USER', id})}>삭제</button>
         </div>
     );
 });
 
-function UserList({users, onRemove, onToggle}) {
+function UserList({users}) {
 
     return (
         <div>
             {
-                users.map(user => (<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />)) // key할 값이 없다면 index 삽
+                users.map(user => (<User user={user} key={user.id} />)) // key할 값이 없다면 index 삽
             }
         </div>
     );
