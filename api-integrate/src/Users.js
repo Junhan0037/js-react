13de883b650742d55bf841af000905e540904000
Junhan0037 <wnsgks0037@naver.com>
2021-06-15 19:52:53 +0900

    feat: useAsync 커스텀 Hook 만들어서 사용하기
    - 커스텀 Hook

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,68 +1,33 @@
-import React, {useEffect, useReducer} from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
+import useAsync from "./useAsync";
+import User from "./User";
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null,
-            }
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null,
-            }
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error,
-            }
-        default:
-            return new Error(`Unhandled action type: ${action.type}`);
-    }
+async function getUsers() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
+    return response.data;
 }
 
 function Users() {
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null,
-    });
-    const fetchUsers = async () => {
-        dispatch({type: 'LOADING'});
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-            dispatch({type: 'SUCCESS', data: response.data});
-        } catch (e) {
-            console.log(e.response.status);
-            dispatch({type: 'ERROR', error: e});
-        }
-    };
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
+    const [state, refetch] = useAsync(getUsers, [], true);
+    const [userId, setUserId] = useState(null);
     const {loading, data: users, error} = state;
 
     if (loading) return <div>로딩중..</div>;
     if (error) return <div>에러가 발생했습니다.</div>
-    if (!users) return null;
+    if (!users) return <button onClick={refetch}>불러오기</button>;
 
     return (
         <>
             <ul>
                 {users.map(user => (
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => setUserId(user.id)}>
                         {user.username} ({user.name})
                     </li>
                 ))}
             </ul>
-            <button onClick={fetchUsers}>다시 불러오기</button>
+            <button onClick={refetch}>다시 불러오기</button>
+            {userId && <User id={userId} />}
         </>
     );
 }
