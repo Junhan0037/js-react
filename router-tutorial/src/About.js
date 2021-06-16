07834332154f7ff27ee702f14fec6aaae33939a4
Junhan0037 <wnsgks0037@naver.com>
2021-06-16 14:07:59 +0900

    feat: 파라미터와 쿼리
    - match
    - location

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import qs from 'qs';
 
-function About() {
+function About({location}) { // location: Route에서 내려주는 쿼리를 가지고 있다.
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.detail === 'true'; // 문자열로 추출된다.
+
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제 프로젝트입니다.</p>
+      {detail && <p>detail값이 true입니다!</p>}
     </div>
   );
 }
---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const profileData = {
+  velopert: {
+    name: '김민준',
+    description: 'Frontend Enginner @ RIDI'
+  },
+  homer: {
+    name: '호머 심슨',
+    description: '심슨 가족에 나오는 아빠 역할 캐릭터'
+  }
+}
+
+function Profile({match}) { // match: Route에서 내려주는 파라미터를 가지고 있다.
+  const {username} = match.params;
+  const profile = profileData[username];
+
+  if (!profile) {
+    return <div>존재하지 않는 사용자입니다.</div>
+  }
+
+  return (
+    <div>
+      <h3>{username} ({profile.name})</h3>
+      <p>
+        {profile.description}
+      </p>
+    </div>
+  );
+}
+
+export default Profile;
