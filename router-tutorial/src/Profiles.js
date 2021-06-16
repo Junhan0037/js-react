b12148f2a4d1c1b0b22d799edf38445d78ad42c6
Junhan0037 <wnsgks0037@naver.com>
2021-06-16 15:32:10 +0900

    feat: 라우터 부가기능
    - Switch
    - NavLink

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Profile from "./Profile";
-import {Link, Route} from 'react-router-dom';
+import {NavLink, Route} from 'react-router-dom';
 
 function Profiles() {
   return (
     <div>
       <h3>사용자 목록</h3>
       <ul>
-        <li><Link to="/profiles/velopert">velopert</Link></li>
-        <li><Link to="/profiles/homer">homer</Link></li>
+        <li><NavLink
+          to="/profiles/velopert"
+          activeStyle={{background: 'black', color: 'white'}}
+          activeClassName="active">
+          velopert
+        </NavLink></li>
+        <li><NavLink to="/profiles/homer" activeStyle={{background: 'black', color: 'white'}}>homer</NavLink></li>
       </ul>
 
       <Route path="/profiles" render={() => <div>사용자를 선택해주세요.</div>} exact />
