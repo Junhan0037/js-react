ef2e3d0cd545062c22e9e58b02323c0687e5947b
Junhan0037 <wnsgks0037@naver.com>
2021-06-17 14:04:48 +0900

    feat: 리덕스 모듈 만들기
    - redux
    - react-redux

---
+++ learn-redux/src/modules/counter.js
@@ -0,0 +1,37 @@
+const SET_DIFF = 'counter/SER_DIFF'; // 이름 중복을 방지하기 위해 prefix 삽입
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+// Action 생성함수
+export const setDiff = diff => ({type: SET_DIFF, diff});
+export const increase = () => ({type: INCREASE});
+export const decrease = () => ({type: DECREASE});
+
+// 초기값
+const initialState = {
+  number: 0,
+  diff: 1,
+};
+
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case SET_DIFF:
+      return {
+        ...state,
+        diff: action.diff,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        number: state.number + state.diff,
+      };
+    case DECREASE:
+      return {
+        ...state,
+        number: state.number - state.diff,
+      };
+    default:
+      return state;
+  }
+};
+
