af0f77447a942b63f3db62809e78fabce38854b0
Junhan0037 <wnsgks0037@naver.com>
2021-06-21 13:46:53 +0900

    feat: redux-thunk
    -

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -8,8 +8,9 @@ import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./modules";
 import logger from 'redux-logger';
 import {composeWithDevTools} from "redux-devtools-extension";
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk, logger)));
 
 ReactDOM.render(
   <React.StrictMode>
