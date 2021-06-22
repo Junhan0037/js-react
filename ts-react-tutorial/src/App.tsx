eb94f6f9db3abe4500baba15cb90c5e413434f94
Junhan0037 <wnsgks0037@naver.com>
2021-06-22 15:32:52 +0900

    feat: 타입스크립트로 리액트 상태 관리하기
    -

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import MyForm from "./MyForm";
+import Counter from "./Counter";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
   const onSubmit = (form: {name: string; description: string;}) => {
     console.log(form);
   };
-  return <MyForm onSubmit={onSubmit} />;
+  return (
+    <>
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+      <ReducerSample />
+    </>
+  );
 }
 
 export default App;
