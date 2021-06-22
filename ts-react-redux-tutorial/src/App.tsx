7f6926cfa007e8ea13dba37964a52feeb721540c
Junhan0037 <wnsgks0037@naver.com>
2021-06-22 16:42:35 +0900

    feat: TypeScript 에서 리덕스 프로처럼 사용하기
    - TodoList

--- ts-react-redux-tutorial/src/App.tsx
+++ ts-react-redux-tutorial/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import CounterContainer from "./containers/CounterContainer";
+import TodoApp from "./containers/TodoApp";
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <br />
+      <TodoApp />
     </div>
   );
 }
