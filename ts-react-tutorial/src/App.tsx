2750f7fc1b170517a7aaaef73fe1c461c79db4ab
Junhan0037 <wnsgks0037@naver.com>
2021-06-22 15:06:25 +0900

    feat: 타입스크립트로 리액트 상태 관리
    - useState
    - 이벤트

---
+++ ts-react-tutorial/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyForm from "./MyForm";
+
+const App: React.FC = () => {
+  const onSubmit = (form: {name: string; description: string;}) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
+}
+
+export default App;
