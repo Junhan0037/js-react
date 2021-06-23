243f868aae110a76863030ea26716b3d15df749b
Junhan0037 <wnsgks0037@naver.com>
2021-06-23 14:58:54 +0900

    feat: 상품 선택과 삭제 주문
    - hooks
    - contexts
    - providers

---
+++ prototype-shop/src/hooks/usePrototypes.js
@@ -0,0 +1,7 @@
+import React, {useContext} from "react";
+import AppStateContext from "../contexts/AppStateContext";
+
+export default function usePrototypes() {
+  const {prototypes} = useContext(AppStateContext);
+  return prototypes;
+}
